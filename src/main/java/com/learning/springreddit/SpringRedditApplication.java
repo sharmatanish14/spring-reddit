b6fa4ae8a61c997a80f6f5b8eb4a5fa473f3532d
Tanish <no email>
2024-02-24 13:56:59 +0530

    initial commit

---
+++ src/main/java/com/learning/springreddit/SpringRedditApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springreddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/springreddit/SpringRedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springreddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
